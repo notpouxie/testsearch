6586ffdb040bfa9cf4252f34ac4f0c1dfc90becb
notpouxie <73078800+notpouxie@users.noreply.github.com>
2022-11-25 13:41:14 +0700

    Delete src directory

--- src/testsearch/testsearch--0.0.1.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION pg_dot_product(float[], float[])
-RETURNS float AS 'MODULE_PATHNAME','testsearch'
-LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION pg_run_session(text)
-RETURNS float[] AS 'MODULE_PATHNAME','testsearch'
-LANGUAGE C STRICT;
-
